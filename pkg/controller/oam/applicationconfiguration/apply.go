e62304d73266a09127a938894071f471349501c7
Ryan Zhang <yangzhangrice@hotmail.com>
2020-06-30 01:09:15 -0700

    add check

--- pkg/controller/oam/applicationconfiguration/apply.go
+++ pkg/controller/oam/applicationconfiguration/apply.go
@@ -60,6 +60,9 @@ type workloads struct {
 }
 
 func (a *workloads) Apply(ctx context.Context, status []v1alpha2.WorkloadStatus, w []Workload, ao ...resource.ApplyOption) error {
+	if len(w) == 0 {
+		return errors.New("the application has no component")
+	}
 	// they are all in the same namespace
 	var namespace = w[0].Workload.GetNamespace()
 	for _, wl := range w {
